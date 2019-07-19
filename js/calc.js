48ae9dc9c6e0af4aad848ab8cfe3a24c47b0e399
Umwali Clara <umwaliclara@gmail.com>
2019-07-19 20:24:38 +0200

    add script in js

--- js/calc.js
+++ js/calc.js
@@ -4,5 +4,16 @@ function address() {
     var YY = parseFloat(document.getElementById("bd").value);
     var CC = (YY - 1) / 100 + 1;
     var birthDay =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-    document.getElementById("disp").innerHTML = Math.round( birthDay);
+    document.getElementById("disp").innerHTML = Math.floor(birthDay);
+
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var gender = document.getElementById("name").value;
+
+    if (gender === "Male"){
+        document.getElementById("name").innerHTML =Male[birthDay];
+    } 
+    else{
+        document.getElementById("name").innerHTML =female[birthDay];
+    }
 }
